222a77172d6722c33c0a6c86a440c0d69233b4f5
이백행 <dlqorgod@naver.com>
2022-12-11 19:29:51 +0900

    deploy.sh

---
+++ 2022/12/11/deploy.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+CURRENT_PID=$(pgrep -fl apache-tomcat-8.5.65-01 | grep java | awk '{print $1}')
+echo "현재 구동 중인 톰캣 pid 확인: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "현재 구동 중인 톰캣이 없으므로 종료하지 않습니다."
+else
+        echo "톰캣 $CURRENT_PID 종료"
+fi
