97a91977a3a95a6e13144bbd0e9971619357a88c
이백행 <dlqorgod@naver.com>
2024-01-30 06:59:55 +0900

    시큐어코딩

--- 2024/contribution/03.[기능추가]DAO단위테스트/MapTeamDAO.sql
+++ 2024/contribution/03.[기능추가]DAO단위테스트/MapTeamDAO.sql
@@ -4,3 +4,12 @@ SELECT /* MapTeamDAO.selectList 지식맵(조직별) 조회(멀티건) */
  WHERE 1 = 1
  ORDER by ORGNZT_ID DESC /* `ORGNZT_ID` char(20) NOT NULL COMMENT '조직ID', */
 ;
+
+SELECT /* MapTeamDAO.selectMapTeamList */ A.ORGNZT_ID , A.ORGNZT_NM , DATE_FORMAT(A.CL_DE,'%Y-%m-%d') 
+CL_YMD , A.KNWLDG_URL KNO_URL FROM COMTNDAMMAPTEAM A WHERE 1 = 1
+-- AND A.ORGNZT_NM like CONCAT('%', 'test 조직명 202401', '%')
+-- AND A.ORGNZT_NM like CONCAT('%', 'TEST 조직명 202401', '%')
+-- AND BINARY(A.ORGNZT_NM) like CONCAT('%', 'test 조직명 202401', '%')
+AND BINARY(A.ORGNZT_NM) like CONCAT('%', 'TEST 조직명 202401', '%')
+LIMIT 10 OFFSET 0 
+;
