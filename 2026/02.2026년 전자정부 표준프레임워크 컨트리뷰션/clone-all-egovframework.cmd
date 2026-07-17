ccafe931bdcdb8a9fc6e06c651ee7ff9169a5884
이백행 <dlqorgod@naver.com>
2026-07-17 23:43:00 +0900

    no message

---
+++ 2026/02.2026년 전자정부 표준프레임워크 컨트리뷰션/clone-all-egovframework.cmd
@@ -0,0 +1,31 @@
+git clone https://github.com/itcen-entec-2026/egovframe-ai-rag.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-boot-sample-java-config.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-common-components.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-development.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-enterprise-business-template.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-ex-cloud-data-stream.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-hands-on-guide.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-mobile-device-api.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-mobile-device-api-legacy.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-msa-common-components.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-msa-edu.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-portal-site-template.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-runtime.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-simple-homepage-template.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-template-simple-backend2.git
+
+git clone https://github.com/itcen-entec-2026/egovframe-web-sample.git
